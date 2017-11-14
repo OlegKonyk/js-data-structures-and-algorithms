cab46196e9985eeaed9f3f097564e2f5798ad5c7
Oleg Konyk <okonyk@apple.com>
2017-11-14 07:49:42 -0800

    queu, stack and recursion (notes only)

--- Stack/stack.js
+++ Stack/stack.js
@@ -1,37 +1,56 @@
 // use only String as a storage
 
-var Stack = function() {
-    this.storage = '';
+class Stack {
+    constructor() {
+        this.storage = "";
+        this.maxSize = "7";
+    }
+
+    push(item) {
+        if (typeof item != 'string') {
+            throw new Error('Input has to be type of String')
+        }
+        if (!this.storage) {
+            this.storage = item;
+        } else {
+            this.storage = this.storage + '|' + item;
+        }
+        console.log('push', this.storage)
+    }
+
+    pop() {
+        var lastElementInd = this.storage.lastIndexOf('|');
+        var popedStr = this.storage.slice(lastElementInd);
+        this.storage = this.storage.substring(0, lastElementInd);
+        return popedStr.substring(1);
+    }
+
+    pick() {
+        var lastElementInd = this.storage.lastIndexOf('|');
+        var popedStr = this.storage.slice(lastElementInd);
+        this.storage = this.storage.substring(0, lastElementInd);
+        return popedStr.substring(1);
+    }
+
+    size() {
+        var size = 0;
+        var separators = (this.storage.match(/\|/g) || []).length;
+        console.log('separators', separators)
+        if (separators) {
+            size = separators+1;
+        }
+        return size;
+    }
+
+    isFull() {
+
+    }
+
+    isEmpty() {
+
+    }
 }
 
-Stack.prototype.size = function () {
-    var size = 0;
-    var separators = (this.storage.match(/\|/g) || []).length;
-    console.log(separators, separators)
-    if (separators) {
-        size = separators+1;
-    }
-    return size;
-}
-
-Stack.prototype.push = function (item) {
-    if (typeof item != 'string') {
-        throw new Error('input has to be type of String')
-    }
-    if (!this.storage) {
-        this.storage = item;
-    } else {
-        this.storage = this.storage + '|' + item;
-    }
-    console.log('push', this.storage)
-}
-
-Stack.prototype.pop = function () {
-    var lastElementInd = this.storage.lastIndexOf('|');
-    var popedStr = this.storage.slice(lastElementInd);
-    this.storage = this.storage.substring(0, lastElementInd);
-    return popedStr.substring(1);
-}
 
 var myWeeklyMeny = new Stack();
 
