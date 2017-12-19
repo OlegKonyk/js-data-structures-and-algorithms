d294242240ff6eebd7b36153bec3bfa9c82bbbaf
Oleg Konyk <okonyk@apple.com>
2017-12-19 13:19:51 -0800

    recursion: fibonacci&factorial

--- Recursion/exercises.js
+++ Recursion/exercises.js
@@ -50,10 +50,37 @@ function exponentLoop(base, expo) {
     return value;
 } 
 
-console.log(exponentLoop(5, -3))
+//console.log(exponentLoop(5, -3))
 
 //4. Write a function 'RecursiveExponent' that takes two arguments base, and expo, recursively returns exponent value of the base.
 
 //5. Write a function 'recursiveMultiplier' that takes two arguments, 'arr and num', and multiplies each arr value into by num and returns an array of the values.
 
 //6. Write a function 'recursiveReverse' that takes an array and uses recursion to return its contents in reverse
+
+
+// #######################
+
+// Fibonacci secuence: every number after the first two is the sum of the two preceding ones
+// 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+
+function fibonacci(n) {
+    if(n <= 2) return 1;
+    return fibonacci(n-1) + fibonacci(n-2)
+}
+
+//console.log(fibonacci(9999));
+
+
+// Factorial
+
+// 5! => 5*4*3*2*1 = 120;
+
+function factorial(n) {
+    if (n <= 1) {
+        return 1;
+    }
+    return factorial(n-1)*n;
+}
+
+console.log('>>>', factorial(5))
